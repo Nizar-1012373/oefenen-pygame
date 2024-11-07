4beaeff4bcfb14204a2ac4d5dc15ab31a90dd11f
Nizar Haddouch <1012373@hr.nl>
2024-11-07 18:38:02 +0100

    give length to array

--- algorithms.py
+++ algorithms.py
@@ -1,24 +1,15 @@
 class Array:
     def __init__(self, length, value):
 
-        self.items = [value]
+        self.items = [value] * length
         self.length = length
 
-    # def check_len(self):
-
-    def length_of_array(self):
-        new = self.items * self.length
-        print(new)
-
     def print(self):
-        i = 0
-
         for i in self.items:
-
             print(i)
 
 
 array2 = Array(8, 0)
 
 
-array2.length_of_array()
+array2.print()
