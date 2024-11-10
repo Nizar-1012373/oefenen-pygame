89c6e4d0fab099ad3dad3988a669d557995c76ab
Nizar Haddouch <1012373@hr.nl>
2024-11-11 00:10:44 +0100

    check if length is incremented by 1 by each calll (does not work yet)

--- algorithms.py
+++ algorithms.py
@@ -1,15 +1,24 @@
 class Array:
     def __init__(self, length, value):
 
-        self.items = [value] * length
         self.length = length
+        self.items = [value] * self.length
+
+    def insert_in_array(self):
+        self.length += 1
+
+        return self.length
+
+    def check_length_is_more(self):
+        if self.insert_in_array():
+            return self.items
 
     def print(self):
         for i in self.items:
             print(i)
 
 
-array2 = Array(8, 0)
+array2 = Array(3, 0)
 
-
+array2.check_bigger()
 array2.print()
