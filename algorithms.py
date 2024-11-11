40d550cd5c8ff9309cc8728f8912f89955fb6e23
Nizar Haddouch <1012373@hr.nl>
2024-11-11 12:52:33 +0100

    inserted one index in to array

--- algorithms.py
+++ algorithms.py
@@ -2,7 +2,7 @@ class Array:
     def __init__(self, length, value):
 
         self.length = length
-        self.items = [value] * self.length
+        self.items = [value] * self.insert_in_array()
 
     def insert_in_array(self):
         self.length += 1
@@ -11,14 +11,17 @@ class Array:
 
     def check_length_is_more(self):
         if self.insert_in_array():
+
             return self.items
 
+    # def removeAt(self):
+
     def print(self):
-        for i in self.items:
+        for i in self.check_length_is_more():
             print(i)
 
 
-array2 = Array(3, 0)
+array2 = Array(5, 0)
 
-array2.check_bigger()
+array2.check_length_is_more()
 array2.print()
