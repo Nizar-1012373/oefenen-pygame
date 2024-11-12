07f564cca5d2d10ab0aa6335c7fbfad9a9cf5808
Nizar Haddouch <1012373@hr.nl>
2024-11-12 19:58:36 +0100

    insert method finally works as expected

--- algorithms.py
+++ algorithms.py
@@ -1,24 +1,21 @@
 class Array:
-    def __init__(self, value, length2):
+    def __init__(self, items, length2):
 
-        self.length2 = [length2]
-        # self.items = [value] * self.removeAt()
-        self.more_items = [value] * self.insert_in_array(0)
+        self.length2 = length2
 
-    def insert_in_array(self, value):
-        self.length2[1] += 1
+        self.items = items
 
-        return self.length2[value]
+    def insert(self, item):
 
-    def print(self):
-        for i in self.insert_in_array(1):
-            i
-        print(self.more_items)
+        for i in range(self.length2):
+
+            self.items += [item]
+        print(self.items)
 
 
-array2 = Array(8, 1)
+array2 = Array([], 1)
 
 
-# array2.print_less()
-array2.print()
-# array2.print()
+array2.apply(6)
+array2.apply(2)
+array2.apply(7)
