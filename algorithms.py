86244bdbf5fb30a1bc4c32d72dc7c1987f39bafd
Nizar Haddouch <1012373@hr.nl>
2024-11-12 20:41:02 +0100

    print function

--- algorithms.py
+++ algorithms.py
@@ -1,21 +1,22 @@
 class Array:
-    def __init__(self, items, length2):
+    def __init__(self, length2):
 
         self.length2 = length2
 
-        self.items = items
+        self.items = []
 
     def insert(self, item):
-
         for i in range(self.length2):
-
             self.items += [item]
+
+    def print(self):
+
         print(self.items)
 
 
-array2 = Array([], 1)
+array2 = Array(1)
 
 
-array2.insert(6)
-array2.insert(2)
-array2.insert(7)
+array2.insert(9)
+array2.insert(93)
+array2.insert(93)
