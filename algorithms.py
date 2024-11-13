a97dd5326410ff7f83128c11cc04bc922467e9c3
Nizar Haddouch <1012373@hr.nl>
2024-11-13 13:55:53 +0100

    check if array is string

--- algorithms.py
+++ algorithms.py
@@ -7,7 +7,16 @@ class Array:
 
     def insert(self, item):
         for i in range(self.length2):
-            self.items += [item]
+            if self.check_if_string(item):
+
+                self.items += [item]
+        return self.items
+
+    def check_if_string(self, item):
+        if type(item) == str:
+            return False
+        if type(item) == int:
+            return True
 
     def print(self):
 
@@ -16,7 +25,13 @@ class Array:
 
 array2 = Array(1)
 
-
+array2.insert(93)
 array2.insert(9)
-array2.insert(93)
-array2.insert(93)
+array2.insert("33")
+
+
+array2.insert(933)
+array2.insert(933)
+array2.insert(34)
+array2.insert(933)
+array2.print()
